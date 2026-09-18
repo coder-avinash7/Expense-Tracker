9036f35dea57a42b3ecb216bc9a7c783a57e3c91
coder-avinash <avinashraj22148297@gmail.com>
2026-09-18 20:45:39 +0530

    Fix login input styling

--- frontend/src/components/Inputs/Input.jsx
+++ frontend/src/components/Inputs/Input.jsx
@@ -30,7 +30,7 @@ const Input = ({ value, onChange, placeholder, label, type }) => {
                 <input
                     type={type == 'password' ? showPassword ? 'text' : 'password' : type}
                     placeholder={placeholder}
-                    className='w-full bg-transparent outline-none'
+                    className='w-full bg-transparent outline-none px-[14px] text-slate-100'
                     value={value}
                     onChange={(e) => onChange(e)}
                 />
